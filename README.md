9d049b68e9a7ff53cc4a9fd8683348b1fd0f71d4
waitig <waitig199212@gmail.com>
2019-09-24 22:59:33 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # js-es6-example
-针对js es6规范的代码示例
+>针对js es6规范的代码示例
+
+运行本示例请使用严格es6模式
+
+如使用WebStrom开发，请在以下设置中，将版本配置为ES6
+>Preferences > Languages & Frameworks > JavaScript
---
+++ string-example.js
@@ -0,0 +1,19 @@
+const {log} = console
+
+// ES6 加强了对 Unicode 的支持,只要将码点放入大括号，就能正确解读该字符
+log('\u{20BB7}') // "𠮷"
+log('\u{41}\u{42}\u{43}') // "ABC"
+log('\u{1F680}' === '\uD83D\uDE80') // true
+let hello = 123;
+// log(hell\u{6F}) // 123
+log('\z' === 'z') // true
+log('\172' === 'z')// true
+log('\x7A' === 'z') // true
+log('\u007A' === 'z') // true
+log('\u{7A}' === 'z') // true
+
+// 字符串的遍历接口
+for (let codePoint of 'foo') {
+    log(codePoint)
+}
+
